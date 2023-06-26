8a01479d1b1f36412db6a7b1945933f17f5cc4bd
ericcbonet <ericcbonet@gmail.com>
2023-06-26 23:08:59 +0200

    wip: setup with docker

--- migrations/20230626194115_create_news_articles.sql
+++ migrations/20230626194115_create_news_articles.sql
@@ -1,10 +1,19 @@
 CREATE TABLE news_articles (
    id SERIAL PRIMARY KEY,
-   title TEXT NOT NULL,
-   category TEXT NOT NULL,
-   date TIMESTAMP NOT NULL,
+   title TEXT,
+   domain TEXT,
+   country TEXT,
+   seen_at TIMESTAMP,
    url TEXT NOT NULL,
-   domain TEXT NOT NULL,
-   language TEXT NOT NULL,
-   country TEXT NOT NULL
+   language TEXT
+);
+
+CREATE TABLE categories (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE news_article_categories (
+     news_article_id INT REFERENCES news_articles(id),
+     category_name TEXT REFERENCES categories(name),
+     PRIMARY KEY (news_article_id, category_name)
 );
